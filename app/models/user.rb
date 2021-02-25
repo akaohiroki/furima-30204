3d7b92357331df59bcffb7854e089aee72bb6670
akaohiroki <akayoshi222@gmail.com>
2021-02-25 12:10:18 +0900

    バリデーションの設定(password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :nickname, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
