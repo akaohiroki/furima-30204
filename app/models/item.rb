11e3fadc6323415f14fc9e48e2622cd648ccf8ab
akaohiroki <akayoshi222@gmail.com>
2021-02-27 12:17:21 +0900

    ActiveHashの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,23 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, length: { maximum: 40 }
     validates :description, length: { maximum: 1000}
-    validates :category_id
-    validates :condition_id
-    validates :shipping_charges_id
-    validates :prefecture_id
-    validates :shipping_days_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charges_id
+      validates :prefecture_id
+      validates :shipping_days_id
+    end
     validates :price, format: {with: /\A[0-9]+\z/ }, numericality: { :greater_than_or_equal_to => 300 }, numericality: { :less_than_or_equal_to => 9999999 }
     validates :image
   end
 
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charges
+  belongs_to :Prefecture
+  belongs_to :shipping_days
 end
