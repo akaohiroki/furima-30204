a5719bdeaa0b1dccd49bbc9d4b3f44112fe9b8d0
akaohiroki <akayoshi222@gmail.com>
2021-03-01 18:12:25 +0900

    データベースへ商品情報の保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charges_id, :prefecture_id, :shipping_days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,9 @@ class Item < ApplicationRecord
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :condition_id
-      validates :shipping_charges_id
+      validates :shipping_charge_id
       validates :prefecture_id
-      validates :shipping_days_id
+      validates :shipping_day_id
     end
     validates :price, format: {with: /\A[0-9]+\z/ }, numericality: { :greater_than_or_equal_to => 300 }, numericality: { :less_than_or_equal_to => 9999999 }
     validates :image
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :shipping_charges
+  belongs_to :shipping_charge
   belongs_to :Prefecture
-  belongs_to :shipping_days
+  belongs_to :shipping_day
 end
