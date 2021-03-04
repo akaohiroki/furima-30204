b9ac765f56d10d510a94ef595176234ff269d4aa
akaohiroki <akayoshi222@gmail.com>
2021-03-04 18:24:41 +0900

    Formオブジェクトの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render: item_orders_path
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end
